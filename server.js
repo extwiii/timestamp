4521096623b6807be48bf8d04cc1d709474ac994
Bilal CAGIRAN <bcagiran@hotmail.com>
2016-09-05 16:42:46 +0100

    fixes port

--- server.js
+++ server.js
@@ -65,8 +65,10 @@ app.get('/', function (req, res) {
 		res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000; 
+
+app.listen(port, function () {
+  console.log('Node.js listening on port ' + port);
 });
 
 
